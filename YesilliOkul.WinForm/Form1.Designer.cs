da47eda156e5306c5643ae05364d142720edd4f0
Murat Bilginer <41648384+MuratBilginerSoft@users.noreply.github.com>
2021-10-20 07:12:00 +0300

    Add files via upload

---
+++ YesilliOkul.WinForm/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace YesilliOkul.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.cmbSinifi = new System.Windows.Forms.ComboBox();
+            this.clbDersleri = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Doğum Tarihi : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınıfı : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dersleri : ";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(117, 49);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(150, 20);
+            this.txtAdi.TabIndex = 1;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(117, 78);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(150, 20);
+            this.txtSoyadi.TabIndex = 2;
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(117, 110);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(150, 20);
+            this.dtpDogumTarihi.TabIndex = 3;
+            // 
+            // cmbSinifi
+            // 
+            this.cmbSinifi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSinifi.FormattingEnabled = true;
+            this.cmbSinifi.Location = new System.Drawing.Point(117, 145);
+            this.cmbSinifi.Name = "cmbSinifi";
+            this.cmbSinifi.Size = new System.Drawing.Size(150, 21);
+            this.cmbSinifi.TabIndex = 4;
+            // 
+            // clbDersleri
+            // 
+            this.clbDersleri.FormattingEnabled = true;
+            this.clbDersleri.Location = new System.Drawing.Point(369, 37);
+            this.clbDersleri.Name = "clbDersleri";
+            this.clbDersleri.Size = new System.Drawing.Size(144, 109);
+            this.clbDersleri.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(533, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 190);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(648, 298);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "TC : ";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(117, 23);
+            this.txtTC.Mask = "00000000000";
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(150, 20);
+            this.txtTC.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 500);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clbDersleri);
+            this.Controls.Add(this.cmbSinifi);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+        private System.Windows.Forms.ComboBox cmbSinifi;
+        private System.Windows.Forms.CheckedListBox clbDersleri;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox txtTC;
+    }
+}
+
---
+++ YesilliOkul.WinForm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YesilliOkul.Business;
+using YesilliOkul.Entity;
+
+namespace YesilliOkul.WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OgrenciBusiness busiOgrenci;
+        SinifBusiness busiSinif;
+        DersBusiness busiDers;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            busiOgrenci = new OgrenciBusiness();
+            busiSinif = new SinifBusiness();
+            busiDers = new DersBusiness();
+
+            dataGridView1.DataSource = busiOgrenci.HepsiniGetir();
+
+            foreach (Sinif item in busiSinif.HepsiniGetir())
+            {
+                cmbSinifi.Items.Add(item);
+            }
+
+            foreach (Ders item in busiDers.HepsiniGetir())
+            {
+                clbDersleri.Items.Add(item);
+            }
+
+            //2milyar170mil = 2.000.000.000
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci o = new Ogrenci();
+            o.TC = Convert.ToInt64(txtTC.Text.Trim());
+            o.Ad = txtAdi.Text.Trim();
+            o.Soyad = txtSoyadi.Text.Trim();
+            o.DogumTarihi = dtpDogumTarihi.Value;
+
+            if (cmbSinifi.SelectedIndex > 0)
+            {
+                o.Sinifi = cmbSinifi.SelectedItem as Sinif; //Unboxing
+                // o.Sinifi = (Sinif)cmbSinifi.SelectedItem  // Casting
+            }
+
+            foreach (Ders item in clbDersleri.CheckedItems)
+            {
+                o.Dersleri.Add(item);
+            }
+
+            busiOgrenci.Ekle(o);
+
+            dataGridView1.DataSource = busiOgrenci.HepsiniGetir();
+        }
+    }
+}
